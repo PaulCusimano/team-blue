271b79358a4de799a0f106268613b06b49db15ca
Khanh Nguyen <kngu221@lsu.edu>
2023-02-23 20:05:04 -0600

    got rid of errors

--- safety-app/src/main/java/com/teamblue/safetyapp/LocationValidation.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/LocationValidation.java
@@ -3,16 +3,7 @@ package com.teamblue.safetyapp;
 public class LocationValidation {
 
 public static void  getLocation() {
-		
-mileLimit =;// max amount of distance to be able to call LSUPD to your location
 
-if getLocation > mileLimit{
-    system.out.println("Call 911")
+    return;
 }
-else{
-    markLocation;// marks location of current user
-    currentLocation = markLocation;
-}
-	}
-return currentLocation;
 }
