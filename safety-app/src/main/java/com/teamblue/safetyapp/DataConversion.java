4d661719c49a6390c88080c3197e9c9959f13eaa
PaulCusimano <pacusimanojr@gmail.com>
2023-02-23 18:41:02 -0600

    Moved Folders

--- safety-app/src/main/java/com/teamblue/safetyapp/DataConversion.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/DataConversion.java
@@ -1,5 +1,21 @@
 package com.teamblue.safetyapp;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataConversion {
 
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static void downloadLatestPDF() throws IOException {
+
+    }
+
+    public static List<String> pdfToString() throws IOException {
+        List<String> result = new ArrayList<>();
+        return result;
+    }
 }
