7fd57bd0d70a636518da3d900ea69339d63afb15
cobyknight <124621304+cobyknight@users.noreply.github.com>
2023-02-23 20:53:56 -0600

    fixed

--- safety-app/src/main/java/com/teamblue/safetyapp/ReportHandler.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/ReportHandler.java
@@ -2,4 +2,13 @@ package com.teamblue.safetyapp;
 
 public class ReportHandler {
 
+    public static void verification(int i){
+
+       return;
+    }
+
+    public static void curLocation(int location){
+
+        return;
+    }
 }
