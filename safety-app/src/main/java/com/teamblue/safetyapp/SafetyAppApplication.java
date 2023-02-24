09b03bffe1e999c4af186f2da688ae1ed6a911bd
PaulCusimano <pacusimanojr@gmail.com>
2023-02-23 20:11:28 -0600

    added firebase package

--- safety-app/src/main/java/com/teamblue/safetyapp/SafetyAppApplication.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/SafetyAppApplication.java
@@ -1,16 +1,17 @@
 package com.teamblue.safetyapp;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
