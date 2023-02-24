9941a65632579b8538e477f687cc9bb0217c5b85
cobyknight <124621304+cobyknight@users.noreply.github.com>
2023-02-23 21:37:28 -0600

    mapManager fix

--- safety-app/src/main/java/com/teamblue/safetyapp/MapManager.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/MapManager.java
@@ -1,5 +1,6 @@
 package com.teamblue.safetyapp;
 
 public class MapManager {
-
+    //apon importing location data from the firebase database, it will then send that location to the google maps api every 24 hours.
+    //This class doesnt actually communicate with any of our other code directly
 }
