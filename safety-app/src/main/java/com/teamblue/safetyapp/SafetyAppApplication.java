3b96887e7c1a7e4c46fc82156311a1fc8760d3eb
PaulCusimano <pacusimanojr@gmail.com>
2023-03-19 17:30:07 -0500

    Added firebase access

--- safety-app/src/main/java/com/teamblue/safetyapp/SafetyAppApplication.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/SafetyAppApplication.java
@@ -1,9 +1,22 @@
 package com.teamblue.safetyapp;
 
-// import com.google.auth.oauth2.GoogleCredentials;
-// import com.google.firebase.FirebaseApp;
-// import com.google.firebase.FirebaseOptions;
-// import com.google.firebase.database.*;
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutures;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.cloud.firestore.v1.FirestoreClient;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import com.teamblue.safetyapp.Models.Location;
 import com.teamblue.safetyapp.Models.Report;
 
@@ -12,7 +25,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -20,8 +40,79 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class SafetyAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+		// You must download the key file (in the discord) and save the path to it in
+		// here everytime you run the code
+		FileInputStream serviceAccount = new FileInputStream(
+				"C:\\Users\\hagri\\Desktop\\campus-safety-294f4-firebase-adminsdk-lc5oa-5c74129f44.json");
+
+		// FirebaseOptions options = FirebaseOptions.builder()
+		// .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+		// .setProjectId("campus-safety-294f4")
+		// .build();
+
+		// FirebaseApp.initializeApp(options);
+
+		// Firestore firestore = FirestoreClient.getFirestore();
+		// DocumentSnapshot snapshot =
+		// firestore.collection("users").document("alice").get().get();
+
+		FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setProjectId("campus-safety-294f4")
+				.build();
+		Firestore db = firestoreOptions.getService();
+
+		DocumentReference docRef = db.collection("users").document("aturing");
+		// Add document data with id "alovelace" using a hashmap
+		Map<String, Object> data = new HashMap<>();
+		data.put("first", "Alan");
+		data.put("last", "Turing");
+		data.put("born", 1912);
+		// asynchronously write data
+		ApiFuture<WriteResult> result = docRef.set(data);
+		// ...
+		// result.get() blocks on response
+		System.out.println("Update time : " + result.get().getUpdateTime());
+
+		// Add document data with auto-generated id.
+		// asynchronously retrieve all users
+		ApiFuture<QuerySnapshot> query = db.collection("users").get();
+		// ...
+		// query.get() blocks on response
+		String firstname = "null";
+		String lastname = "null";
+		Long born = null;
+
+		QuerySnapshot querySnapshot = query.get();
+		List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+		for (QueryDocumentSnapshot document : documents) {
+			System.out.println("User: " + document.getId());
+			System.out.println("First: " + document.getString("first"));
+			if (document.contains("middle")) {
+				System.out.println("Middle: " + document.getString("middle"));
+			}
+			firstname = document.getString("first");
+			lastname = document.getString("last");
+			born = document.getLong("born");
+			System.out.println("Last: " + document.getString("last"));
+			System.out.println("Born: " + document.getLong("born"));
+		}
+
+		DocumentReference docuRef = db.collection("users").document("77FoxF6XLoYAOlGE06zGr94SZ7g2");
+		// asynchronously retrieve the document
+		ApiFuture<DocumentSnapshot> future = docuRef.get();
+		// ...
+		// future.get() blocks on response
+		DocumentSnapshot document = future.get();
+		if (document.exists()) {
+			System.out.println("Document data: " + document.getData());
+		} else {
+			System.out.println("No such document!");
+		}
+
 		SpringApplication.run(SafetyAppApplication.class, args);
+		System.out.println(firstname + " " + lastname + " " + born);
 	}
 
 	/**
