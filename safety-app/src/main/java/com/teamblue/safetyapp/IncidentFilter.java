589c0b4e8a4f5b6084712a749113b06812cef21d
efarkos <124732457+efarkos@users.noreply.github.com>
2023-03-23 15:33:33 -0500

    Added import for Report in IncidentFilter

--- safety-app/src/main/java/com/teamblue/safetyapp/IncidentFilter.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/IncidentFilter.java
@@ -3,6 +3,8 @@ package com.teamblue.safetyapp;
 //import java.io.IOException;
 import java.util.List;
 
+import com.teamblue.safetyapp.Models.Report;
+
 public class IncidentFilter {
     public static void filterIncidents(Report data) {
         // Send to Map Manager if incident is not a non-crime or e-crash
