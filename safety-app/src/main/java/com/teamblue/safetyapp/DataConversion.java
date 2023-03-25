1abe40b440cf71184d59f4132739cd93f3dd61a0
Dylan Scivicque <dylanscivicque@gmail.com>
2023-03-25 02:35:19 -0500

    converts entire PDF to CSV and JSON

--- safety-app/src/main/java/com/teamblue/safetyapp/DataConversion.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/DataConversion.java
@@ -3,42 +3,54 @@ package com.teamblue.safetyapp;
 import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
-import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
 import technology.tabula.*;
+import technology.tabula.extractors.SpreadsheetExtractionAlgorithm;
+import technology.tabula.writers.CSVWriter;
+import technology.tabula.writers.JSONWriter;
 
-//Not tested, but theoretically currently only outputs to console. Need to add CSV and JSON as outputs
-@SuppressWarnings("rawtypes")
 public class DataConversion {
-    public static void pdfConversion() throws IOException {
-        File file = new File("safety-app/src/main/resources/PDcrimelog.pdf");
+
+    // Main method to run the code
+    public static void main(String[] args) throws IOException {
+
+        // Load the PDF file
+        File file = new File("team-blue\\safety-app\\src\\main\\resources\\PDcrimelog.pdf");
         InputStream in = new FileInputStream(file);
+
+        // Load the document and extract the tables
         try (PDDocument document = PDDocument.load(in);
-                ObjectExtractor oe = new ObjectExtractor(document)) {
+             ObjectExtractor oe = new ObjectExtractor(document)) {
+
+            // Use SpreadsheetExtractionAlgorithm to extract tables
             SpreadsheetExtractionAlgorithm sea = new SpreadsheetExtractionAlgorithm();
             PageIterator pi = oe.extract();
+            List<Table> tables = new ArrayList<>();
+
+            // Iterate over the pages of the document
             while (pi.hasNext()) {
-                // iterate over the pages of the document
                 Page page = pi.next();
-                List<Table> table = sea.extract(page);
-                // iterate over the tables of the page
-                for (Table tables : table) {
-                    List<List<RectangularTextContainer>> rows = tables.getRows();
-                    // iterate over the rows of the table
-                    for (List<RectangularTextContainer> cells : rows) {
-                        // print all column-cells of the row plus linefeed
-                        for (RectangularTextContainer content : cells) {
-                            // Note: Cell.getText() uses \r to concat text chunks
-                            String text = content.getText().replace("\r", " ");
-                            System.out.print(text + "|");
-                        }
-                        System.out.println();
-                    }
-                }
+                List<Table> pageTables = sea.extract(page);
+                tables.addAll(pageTables);
             }
+
+            // Convert the tables to JSON format and write to file
+            JSONWriter jsonWriter = new JSONWriter();
+            FileWriter jsonOut = new FileWriter("team-blue\\safety-app\\src\\main\\resources\\JSONoutput.json");
+            jsonWriter.write(jsonOut, tables);
+
+            // Convert the tables to CSV format and write to file
+            CSVWriter csvWriter = new CSVWriter();
+            FileWriter csvOut = new FileWriter("team-blue\\safety-app\\src\\main\\resources\\CSVoutput.csv");
+            csvWriter.write(csvOut, tables);
+
+        } finally {
+            in.close();
         }
     }
 }
