8fa2a2cea7a46305a24bf3404c2bfa5a35b1f067
PaulCusimano <pacusimanojr@gmail.com>
2023-04-04 15:54:46 -0500

    Added FirestoreDatabase class

---
+++ safety-app/src/main/java/com/teamblue/safetyapp/FirestoreDatabase.java
@@ -0,0 +1,47 @@
+package com.teamblue.safetyapp;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
+import com.google.cloud.firestore.GeoPoint;
+import com.google.cloud.firestore.WriteResult;
+import com.teamblue.safetyapp.Models.Report;
+
+public class FirestoreDatabase {
+    // Declare the constants for the service account path and project ID
+    private static final String SERVICE_ACCOUNT_PATH = "C:\\Users\\hagri\\Desktop\\campus-safety-294f4-firebase-adminsdk-lc5oa-5c74129f44.json";
+    private static final String PROJECT_ID = "campus-safety-294f4";
+
+    // Declare a private static field for the Firestore object
+    private static Firestore firestore;
+
+    // Declare a private constructor to prevent instantiation
+    private FirestoreDatabase() {
+    }
+
+    // Declare a public static method to get the Firestore object
+    public static Firestore getFirestore() throws IOException {
+        // Check if the firestore field is null
+        if (firestore == null) {
+            // Read the service account JSON file as an input stream
+            FileInputStream serviceAccount = new FileInputStream(SERVICE_ACCOUNT_PATH);
+            // Build a FirestoreOptions object with the credentials and project ID
+            FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setProjectId(PROJECT_ID)
+                    .build();
+            // Assign the firestore field to the service provided by the options
+            firestore = firestoreOptions.getService();
+        }
+        // Return the firestore field
+        return firestore;
+    }
+}
--- safety-app/src/main/java/com/teamblue/safetyapp/MapManager.java
+++ safety-app/src/main/java/com/teamblue/safetyapp/MapManager.java
@@ -1,26 +1,19 @@
 package com.teamblue.safetyapp;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.GeoPoint;
 import com.google.cloud.firestore.WriteResult;
 import com.teamblue.safetyapp.Models.Report;
 
 // This class manages the communication with the Firestore database
 public class MapManager {
-    // The path to the service account JSON file
-    private static final String SERVICE_ACCOUNT_PATH = "C:\\Users\\hagri\\Desktop\\campus-safety-294f4-firebase-adminsdk-lc5oa-5c74129f44.json";
-    // The project ID of the Firestore database
-    private static final String PROJECT_ID = "campus-safety-294f4";
     // The name of the collection that stores the locations of the reports
     private static final String LOCATIONS_COLLECTION = "locations_document";
     // The name of the collection that stores the details of the reports
@@ -29,7 +22,8 @@ public class MapManager {
     // This method sends a report object to the Firestore database
     public static void sendToDatabase(Report report) throws IOException, InterruptedException, ExecutionException {
         // Get a reference to the Firestore database
-        Firestore db = getFirestoreDatabase();
+        Firestore db = FirestoreDatabase.getFirestore();
+
         // Create a GeoPoint object from the report's location
         GeoPoint location = new GeoPoint(report.getLocation().getLatitude(), report.getLocation().getLongitude());
         // Get a reference to the document that corresponds to the report's name in the
@@ -48,19 +42,5 @@ public class MapManager {
         ApiFuture<WriteResult> future = docRef.set(reportData);
         // Wait for the write operation to complete
         future.get();
-    }
-
-    // This method returns a Firestore object that connects to the database using
-    // the service account credentials and project ID
-    private static Firestore getFirestoreDatabase() throws IOException {
-        // Read the service account JSON file as an input stream
-        FileInputStream serviceAccount = new FileInputStream(SERVICE_ACCOUNT_PATH);
-        // Build a FirestoreOptions object with the credentials and project ID
-        FirestoreOptions firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setProjectId(PROJECT_ID)
-                .build();
-        // Return a Firestore object that provides access to the database
-        return firestoreOptions.getService();
     }
 }
